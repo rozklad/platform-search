2fee4a85a56d4f38639e3861ec1df359de68d1ed
rozklad <jan.rozklad@gmail.com>
2016-05-31 16:08:21 +0200

    automatic commit

--- themes/frontend/default/packages/sanatorium/search/views/hooks/box.blade.php
+++ themes/frontend/default/packages/sanatorium/search/views/hooks/box.blade.php
@@ -1,14 +1,16 @@
-<form method="GET" class="navbar-form {{ $css_classes }}">
-	<div class="input-group">
-		<input type="text" name="{{ trans('sanatorium/shop::general.search.input') }}" class="form-control input-lg product-search" placeholder="{{ trans('sanatorium/search::action.search_placeholder') }}">
-		<span class="input-group-btn">
-			<button class="btn btn-primary btn-lg" type="submit">
-				<span class="button-label">{{ trans('sanatorium/search::action.search_btn') }}</span>
-				<i class="fa fa-search"></i>
-			</button>
-		</span>
+<!-- Widget: sanatorium/search::hooks.box -->
+<form method="GET" class="{{ $css_classes }}">
+	<div class="form-group">
+		<input type="text" name="{{ trans('sanatorium/shop::general.search.input') }}" class="form-control product-search" placeholder="{{ trans('sanatorium/search::general.search_placeholder') }}">
 	</div>
+	<button class="btn btn-default" type="submit">
+		<span class="button-label">
+			{{ trans('sanatorium/search::action.search_btn') }}
+		</span>
+		<i class="fa fa-search"></i>
+	</button>
 
+	{{-- Note down user_id for analytics --}}
 	@if ( Sentinel::check() )
 		@if ( $user = Sentinel::getUser() ) 
 			<input type="hidden" name="user_id" value="{{ $user->id }}">
